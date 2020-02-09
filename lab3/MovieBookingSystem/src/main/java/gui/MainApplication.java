6e5317cc3470d28eb33b6e55e837fed7575c7016
Andreas Nilsson <nilsiker94@gmail.com>
2020-02-09 17:26:06 +0100

    made path linux-friendly

--- lab3/MovieBookingSystem/src/main/java/gui/MainApplication.java
+++ lab3/MovieBookingSystem/src/main/java/gui/MainApplication.java
@@ -24,7 +24,7 @@ public class MainApplication extends Application {
 		try {
 			
 			// BorderPane root = new BorderPane();
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/TopTab.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/topTab.fxml"));
 			Parent root = loader.load();
 			
 			Scene scene = new Scene(root,600,440);
